c2b7b537eecdcca423864c21e7a712ec8da03b3a
Asunqingwen <sqw123az@sina.com>
2019-08-13 08:15:04 +0800

    no completed

---
+++ easy/Palindrome Number.py
@@ -0,0 +1,21 @@
+"""
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+"""
+
+
+def isPalindrome(x: int) -> bool:
+    if x < 0:
+        return False
+    sum_num =
+
+
+if __name__ == '__main__':
+    m = 3
+
+    result = isPalindrome(m)
+    print(result)
