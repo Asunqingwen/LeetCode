9c6e36d682e04618442bf62d75edcd0d5460a67a
Asunqingwen <sqw123az@sina.com>
2019-08-20 08:28:56 +0800

    Merge Two Binary Trees

---
+++ easy/Merge Two Binary Trees.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/20 0020 8:17
+# @Author  : 没有蜡笔的小新
+# @E-mail  : sqw123az@sina.com
+# @FileName: Merge Two Binary Trees.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/Asunqingwen
+# @GitHub  ：https://github.com/Asunqingwen
+
+"""
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Note: The merging process must start from the root nodes of both trees.
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def mergeTrees(t1: TreeNode, t2: TreeNode) -> TreeNode:
+    pass
+
+if __name__ == '__main__':
+    input = "10, 5, 15, 3, 7, null, 18"
+    root = stringToTreeNode(input)
+    output = treeNodeToString(root)
+    print(output)
