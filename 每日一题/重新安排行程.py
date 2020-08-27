64f651f14bab60f1095dc71183456751965a6ed0
sqw@mvtech.cc <sqw123az@sina.com>
2020-08-27 10:49:40 +0800

    重新安排行程.py

---
+++ 每日一题/重新安排行程.py
@@ -0,0 +1,47 @@
+"""
+给定一个机票的字符串二维数组 [from, to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。
+
+说明:
+
+如果存在多种有效的行程，你可以按字符自然排序返回最小的行程组合。例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前
+所有的机场都用三个大写字母表示（机场代码）。
+假定所有机票至少存在一种合理的行程。
+示例 1:
+
+输入: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+输出: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+示例 2:
+
+输入: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+输出: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+解释: 另一种有效的行程是 ["JFK","SFO","ATL","JFK","ATL","SFO"]。但是它自然排序更大更靠后。
+"""
+import heapq
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def dfs(curr: str):
+            while vec[curr]:
+                tmp = heapq.heappop(vec[curr])
+                dfs(tmp)
+            stack.append(curr)
+
+        vec = defaultdict(list)
+        for depart, arrive in tickets:
+            vec[depart].append(arrive)
+        for key in vec:
+            heapq.heapify(vec[key])
+
+        stack = list()
+        dfs("JFK")
+        return stack[::-1]
+
+
+if __name__ == '__main__':
+    tickets = [["EZE","TIA"],["EZE","HBA"],["AXA","TIA"],["JFK","AXA"],["ANU","JFK"],["ADL","ANU"],["TIA","AUA"],["ANU","AUA"],["ADL","EZE"],["ADL","EZE"],["EZE","ADL"],["AXA","EZE"],["AUA","AXA"],["JFK","AXA"],["AXA","AUA"],["AUA","ADL"],["ANU","EZE"],["TIA","ADL"],["EZE","ANU"],["AUA","ANU"]]
+    sol = Solution()
+    result = sol.findItinerary(tickets)
+    print(result)
