e6ee6ccdf5bb3ff1ebb10eea260633f3947837ee
sqw@mvtech.cc <sqw123az@sina.com>
2020-09-25 17:45:27 +0800

    一道简单题

---
+++ 简单/两整数之和.py
@@ -0,0 +1,30 @@
+"""
+不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+
+示例 1:
+
+输入: a = 1, b = 2
+输出: 3
+示例 2:
+
+输入: a = -2, b = 3
+输出: 1
+"""
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        a &= 0xFFFFFFFF
+        b &= 0xFFFFFFFF
+        while b:
+            carry = a & b
+            a ^= b
+            b = ((carry) << 1) & 0xFFFFFFFF
+        return a if a < 0x80000000 else ~(a ^ 0xFFFFFFFF)
+
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    sol = Solution()
+    print(sol.getSum(a, b))
