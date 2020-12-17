1a22f296bb2005c3126846ac3fb56d65ee97025a
sqw@mvtech.cc <sqw123az@sina.com>
2020-12-17 13:07:14 +0800

    三道简单题

---
+++ 简单/扑克牌中的顺子.py
@@ -0,0 +1,42 @@
+'''
+从扑克牌中随机抽5张牌，判断是不是一个顺子，即这5张牌是不是连续的。2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+
+ 
+
+示例 1:
+
+输入: [1,2,3,4,5]
+输出: True
+ 
+
+示例 2:
+
+输入: [0,0,1,2,5]
+输出: True
+ 
+
+限制：
+
+数组长度为 5 
+
+数组的数取值为 [0, 13] .
+'''
+from typing import List
+
+
+class Solution:
+    def isStraight(self, nums: List[int]) -> bool:
+        nums.sort()
+        zero = 0
+        for i in range(4):
+            if nums[i] == 0:
+                zero += 1
+            elif nums[i] == nums[i+1]:
+                return False
+        return nums[-1] - nums[zero] < 5
+
+
+if __name__ == '__main__':
+    nums = [0, 0, 1, 2, 5]
+    sol = Solution()
+    print(sol.isStraight(nums))
