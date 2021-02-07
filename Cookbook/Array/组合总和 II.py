ae1afc5b8dc2208d8fcf1c8ec7dbd87096c57496
sqw@mvtech.cc <sqw123az@sina.com>
2021-02-07 17:02:36 +0800

    两道题

---
+++ Cookbook/Array/组合总和 II.py
@@ -0,0 +1,63 @@
+'''
+给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的每个数字在每个组合中只能使用一次。
+
+说明：
+
+所有数字（包括目标数）都是正整数。
+解集不能包含重复的组合。 
+示例 1:
+
+输入: candidates = [10,1,2,7,6,1,5], target = 8,
+所求解集为:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+示例 2:
+
+输入: candidates = [2,5,2,1,2], target = 5,
+所求解集为:
+[
+  [1,2,2],
+  [5]
+]
+'''
+from typing import List
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        def helper(candidates, index, target):
+            if target == 0:
+                res.append(ans[:])
+                return
+            for i in range(index, len(candidates)):
+                if target < candidates[i]:
+                    break
+                if i > index and candidates[i] == candidates[i - 1]:
+                    continue
+                ans.append(candidates[i])
+                helper(candidates, i + 1, target - candidates[i])
+                ans.pop()
+            return
+
+        if not candidates:
+            return []
+        res = list()
+        ans = list()
+        candidates.sort()
+        helper(candidates, 0, target)
+        return list(res)
+
+
+if __name__ == '__main__':
+    candidates = [10, 1, 2, 7, 6, 1, 5]
+    target = 8
+    sol = Solution()
+    print(sol.combinationSum2(candidates, target))
---
+++ Cookbook/Array/组合总和.py
@@ -0,0 +1,64 @@
+'''
+给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的数字可以无限制重复被选取。
+
+说明：
+
+所有数字（包括 target）都是正整数。
+解集不能包含重复的组合。 
+示例 1：
+
+输入：candidates = [2,3,6,7], target = 7,
+所求解集为：
+[
+  [7],
+  [2,2,3]
+]
+示例 2：
+
+输入：candidates = [2,3,5], target = 8,
+所求解集为：
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+ 
+
+提示：
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+candidate 中的每个元素都是独一无二的。
+1 <= target <= 500
+'''
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def helper(index=0, sum_=0):
+            if index >= len(candidates) or sum_ >= target:
+                if sum_ == target:
+                    res.append(ans[:])
+                return
+            ans.append(candidates[index])
+            helper(index, sum_ + candidates[index])
+            ans.pop()
+            helper(index + 1, sum_)
+
+        if not candidates:
+            return []
+        res = list()
+        ans = list()
+        helper()
+        return list(res)
+
+
+if __name__ == '__main__':
+    candidates = [2, 3, 6, 7]
+    target = 7
+    sol = Solution()
+    print(sol.combinationSum(candidates, target))
