1ae9935f9dc90d02369102b26afc343e6005df67
sqw@mvtech.cc <sqw123az@sina.com>
2021-04-07 10:34:47 +0800

    一道题

---
+++ Cookbook/Array/寻找旋转排序数组中的最小值.py
@@ -0,0 +1,47 @@
+'''
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] 。
+
+请找出其中最小的元素。
+
+ 
+
+示例 1：
+
+输入：nums = [3,4,5,1,2]
+输出：1
+示例 2：
+
+输入：nums = [4,5,6,7,0,1,2]
+输出：0
+示例 3：
+
+输入：nums = [1]
+输出：1
+ 
+
+提示：
+
+1 <= nums.length <= 5000
+-5000 <= nums[i] <= 5000
+nums 中的所有整数都是 唯一 的
+nums 原来是一个升序排序的数组，但在预先未知的某个点上进行了旋转
+'''
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) >> 1
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
+
+
+if __name__ == '__main__':
+    nums = [3, 4, 5, 1, 2]
+    sol = Solution()
+    print(sol.findMin(nums))
