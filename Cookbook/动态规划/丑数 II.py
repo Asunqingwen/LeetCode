3ba47c268e6fde39b119764d38b934d5eb2debf7
sqw@mvtech.cc <sqw123az@sina.com>
2021-04-11 09:44:00 +0800

    每日一题

---
+++ Cookbook/动态规划/丑数 II.py
@@ -0,0 +1,46 @@
+'''
+给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+
+丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+
+ 
+
+示例 1：
+
+输入：n = 10
+输出：12
+解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+示例 2：
+
+输入：n = 1
+输出：1
+解释：1 通常被视为丑数。
+ 
+
+提示：
+
+1 <= n <= 1690
+'''
+
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        p2 = p3 = p5 = 1
+        for i in range(2, n + 1):
+            num1, num2, num3 = dp[p2] * 2, dp[p3] * 3, dp[p5] * 5
+            dp[i] = min(num1, num2, num3)
+            if dp[i] == num1:
+                p2 += 1
+            if dp[i] == num2:
+                p3 += 1
+            if dp[i] == num3:
+                p5 += 1
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    n = 10
+    sol = Solution()
+    print(sol.nthUglyNumber(n))
