c2278513ea67475057893d59005e94fec5dacc8f
sqw@mvtech.cc <sqw123az@sina.com>
2021-04-27 10:42:12 +0800

    每日一练

---
+++ Cookbook/树/二叉搜索树的范围和.py
@@ -0,0 +1,45 @@
+'''
+给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+
+ 
+
+示例 1：
+
+
+输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+输出：32
+示例 2：
+
+
+输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+输出：23
+ 
+
+提示：
+
+树中节点数目在范围 [1, 2 * 104] 内
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+所有 Node.val 互不相同
+'''
+from Tree import stringToTreeNode, TreeNode
+
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        if not root:
+            return 0
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+
+if __name__ == '__main__':
+    nums = "10,5,15,3,7,13,18,1,null,6"
+    root = stringToTreeNode(nums)
+    low = 6
+    high = 10
+    sol = Solution()
+    print(sol.rangeSumBST(root, low, high))
