b7c59c826bcd17cb1333571eb9f13f5c2b89b4ee
sqw@mvtech.cc <sqw123az@sina.com>
2022-09-01 16:20:37 +0800

    子序列

--- leetcode/editor/cn/[866]回文素数.py
+++
@@ -1,5 +0,0 @@
-
-# leetcode submit region begin(Prohibit modification and deletion)
-class Solution:
-    def primePalindrome(self, n: int) -> int:
-# leetcode submit region end(Prohibit modification and deletion)
